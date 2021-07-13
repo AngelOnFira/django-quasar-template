040a73dc70c261a9443aa8550d83b16c83f30234
Forest Anderson <forestkzanderson@gmail.com>
2021-07-13 18:50:50 -0400

    Basic messages app

--- django/backend/settings.py
+++ django/backend/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    ### THIRD PARTY ###
+    'rest_framework',
+    ### LOCAL ###
+    'chat',
 ]
 
 MIDDLEWARE = [
